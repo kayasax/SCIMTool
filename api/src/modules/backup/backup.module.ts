bfdc1825b9e5eb87c954ed37c3cb2f7fec46c2a2
Loïc MICHEL <loic.michel@gmail.com>
2025-09-30 10:40:38 +0200

    fix: Import OAuthModule in BackupModule for SharedSecretGuard

--- api/src/modules/backup/backup.module.ts
+++ api/src/modules/backup/backup.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BackupService } from './backup.service';
 import { BackupController } from './backup.controller';
+import { OAuthModule } from '../../oauth/oauth.module';
 
 @Module({
+  imports: [OAuthModule],
   providers: [BackupService],
   controllers: [BackupController],
   exports: [BackupService],
